58c248270de7d587704964b5735512d5c50bbc30
Varga Márk Örs <90794458+Varga-Mark-Ors@users.noreply.github.com>
2024-11-23 00:57:04 +0200

    feat: Actor class created

--- Weblap/src/main/java/hu/filmdepot/filmsite/model/Actor.java
+++ Weblap/src/main/java/hu/filmdepot/filmsite/model/Actor.java
@@ -1,4 +1,25 @@
 package hu.filmdepot.filmsite.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jdk.jfr.Enabled;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Actor {
+    @Id
+    @EqualsAndHashCode.Include
+    String idNumber;
+
+    String name;
+    String age;
+    Boolean gender;
+
+    double height;
+    int netWorth;
 }
