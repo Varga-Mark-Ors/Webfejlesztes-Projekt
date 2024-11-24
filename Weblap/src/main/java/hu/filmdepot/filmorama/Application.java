6fedcebaeffc74d31d31e69087e658032d927755
Varga Márk Örs <90794458+Varga-Mark-Ors@users.noreply.github.com>
2024-11-24 12:21:16 +0200

    feat: Cast runner and application.yml created

--- Weblap/src/main/java/hu/filmdepot/filmorama/Application.java
+++ Weblap/src/main/java/hu/filmdepot/filmorama/Application.java
@@ -2,12 +2,14 @@ package hu.filmdepot.filmorama;
 
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SpringBootApplication
 public class Application
         extends SpringBootServletInitializer
         implements WebApplicationInitializer {
---
+++ Weblap/src/main/java/hu/filmdepot/filmorama/CastRunner.java
@@ -0,0 +1,39 @@
+package hu.filmdepot.filmorama;
+
+import hu.filmdepot.filmorama.model.Cast;
+import hu.filmdepot.filmorama.repository.ActorRepository;
+import hu.filmdepot.filmorama.repository.CastRepository;
+import hu.filmdepot.filmorama.repository.MovieRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.stream.Stream;
+
+public class CastRunner
+        implements CommandLineRunner {
+
+    private ActorRepository actorRepository;
+    private MovieRepository movieRepository;
+    private CastRepository castRepository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        final var actor = actorRepository
+                .findById("ACTOR1")
+                .orElseThrow(() -> new IllegalArgumentException("Actor not found"));
+
+        Stream.of("Dreamcatchers", "Comedy Royale", "Haunted Manor", "Whispers of the Sea")
+                .map(code -> {
+                    final var movie = movieRepository
+                            .findById(code)
+                            .orElseThrow(() -> new IllegalArgumentException("Movie not found"));
+                    return Cast.builder()
+                            .actor(actor)
+                            .movie(movie)
+                            .build();
+                })
+                .map(castRepository::save)
+                .forEach(System.out::println);
+    }
+}
