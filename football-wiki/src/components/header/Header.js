8188443064ead5425eec447ed81b08be6ded6d3b
Varga Márk Örs <90794458+Varga-Mark-Ors@users.noreply.github.com>
2024-12-04 18:05:07 +0200

    feat: Creating the Head component

---
+++ football-wiki/src/components/header/Header.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFutbol } from '@fortawesome/free-regular-svg-icons';
+import { Container, NavbarCollapse } from "react-bootstrap"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import { NavLink } from "react-router-dom"
+import Button from "react-bootstrap/Button";
+
+
+const Header = () => {
+    return (
+        <Navbar bg = "dark" variant = "dark" expands = "lg">
+            <Container fluid>
+                <Navbar.Brand href="/" style={{"color":'gold'}}>
+                    <FontAwesomeIcon icon ={faFutbol} /> Gold 
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <NavbarCollapse id = "navbarScroll">
+                    <Nav className="me-auto my-2 my-lg-0"
+                        style={{maxHeight:`100px`}}
+                        navbarScroll
+                    >
+                        <NavLink className="nav-link" to= "/">Home</NavLink>
+                        <NavLink className="nav-link" to= "/">Players</NavLink>
+                    </Nav>
+                    <Button variant="outline-info" className="me-2">Login</Button>
+                    <Button variant="outline-info" className="me-2">Register</Button>
+                </NavbarCollapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
