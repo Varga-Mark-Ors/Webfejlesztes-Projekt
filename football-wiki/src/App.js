3bd3609e8e98ad0844a2266924a345fa46ecaa32
Varga Márk Örs <90794458+Varga-Mark-Ors@users.noreply.github.com>
2024-12-05 01:24:59 +0200

    Fix: Trying to solve the child routes rendering

--- football-wiki/src/App.js
+++ football-wiki/src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
 import Layout from './components/Layout';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Squad from "./components/squad/Squad";
 import Squads from './components/squads/Squads';
 
 function App() {
-  const [teams, setTeams] = useState([]); 
+  const [teams, setTeams] = useState([]);
 
   const getTeams = async () => {
     try {
       const response = await api.get("/team");
-      setTeams(response.data || []); 
+      setTeams(response.data || []);
     } catch (err) {
       console.error("Error fetching teams:", err);
     }
@@ -22,16 +22,19 @@ function App() {
 
   useEffect(() => {
     getTeams();
-  }, []); 
+  }, []);
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="*" element={<Layout/>}>
-          <Route path="/" element={<Home teams={teams}/>}/>
-          <Route path="/team/:teamid" element={<Squad teams={teams}/>}/>
-          <Route path="/team" element={<Squads teams={teams} />} />
+        {/* A szülő útvonal most már *-ot tartalmaz */}
+        <Route path="*" element={<Layout />}>
+          {/* Az index útvonal most relatív */}
+          <Route index element={<Home teams={teams} />} />
+          {/* A gyerek útvonalak relatívak */}
+          <Route path="team/:teamid" element={<Squad teams={teams} />} />
+          <Route path="team" element={<Squads teams={teams} />} />
         </Route>
       </Routes>
     </div>
