c4f6a30fc838e4fe15cccb943091772e063d223c
Varga Márk Örs <90794458+Varga-Mark-Ors@users.noreply.github.com>
2024-12-07 23:07:50 +0200

    refactor: Redesigning the /team page

--- football-wiki/src/components/squads/Squads.js
+++ football-wiki/src/components/squads/Squads.js
@@ -5,18 +5,22 @@ import { Link } from "react-router-dom";
 
 const Squads = ({ teams }) => {
   return (
-    <div className="teams-container-squads">
+    <div className="teams-container">
       {teams.map((team) => (
-        <Paper key={team.name} className="team-row-squads">
-          <div className="team-badge-squads">
+        <Paper
+          key={team.name}
+          className="teams-row"
+          style={{ backgroundColor: "#1a1a1a", color: "#ffc107" }} 
+        >
+          <div className="teams-badge">
             <img src={team.badge} alt={`${team.name} logo`} />
           </div>
           <Link to={`/team/${team.teamId}`}>
-            <div className="team-name-squads">
+            <div className="teams-name">
               <h4>{team.name}</h4>
             </div>
           </Link>
-          <div className="team-details-squads">
+          <div className="teams-details">
             <p><strong>League:</strong> {team.league}</p>
             <p><strong>Country:</strong> {team.country}</p>
             <p><strong>City:</strong> {team.city}</p>
